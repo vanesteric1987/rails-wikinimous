31b5870cf24577ec8a01823d61af4305460ea534
Stephanie Ramiaramanana <vanesteric1987@gmail.com>
2022-05-24 14:00:18 +0100

    rails wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title:    Faker::Company.name,
+    content: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+  )
+  article.save!
+end
+puts 'Finished!'
